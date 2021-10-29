6d0a04a6dd7732f9ae6d4f573351f262a15c0c61
sargun <sargun.narula5@gmail.com>
2021-10-30 03:38:08 +0530

    Modified profile api and set meeting api

--- api/urls.py
+++ api/urls.py
@@ -3,14 +3,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('api/add/',                    views.UserRegister         ),
-    path('api/login/',                  views.Userlogin            ),
-    path('api/list/',                   views.UserSearch           ),
-    path('api/profile/<str:username>/', views.UserDetail.as_view() ),
-    path('api/meetings/',               views.MeetingList.as_view()),
-    path('api/meetings/all/',           views.MeetingAll           ),
-    path('api/meetings/hosted/',        views.MeetingHosted        ),
-    path('api/meetings/invited/',       views.MeetingInvited       ),
+    path('api/users',                   views.Users                ),  # GET  -To get all users list
+    path('api/add/',                    views.UserRegister         ),  # POST -Register
+    path('api/login/',                  views.Userlogin            ),  # POST -Login 
+    path('api/list/',                   views.UserSearch           ),  # POST -Search users while creating room
+    path('api/profile/<str:username>/', views.UserDetail.as_view() ),  # GET  -To get details of user
+
+    path('api/meetings/set/',           views.MeetingSet           ),  # POST -To set a meeting 
+    path('api/meetings/total/',         views.MeetingAll           ),  # POST -To get user    meetings
+    path('api/meetings/hosted/',        views.MeetingHosted        ),  # POST -To get hosted  meetings
+    path('api/meetings/invited/',       views.MeetingInvited       ),  # POST -To get invited meetings
+    path('api/meetings/all/',           views.MeetingList.as_view()),  # POST -To get all     meetings
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -10,76 +10,143 @@ from rest_framework            import generics
 from rest_framework.response   import Response
 from rest_framework.decorators import api_view
 
-import json
+#from backend.api import serializers
 
 # Create your views here.
+
+# To get Users
+@api_view(['GET'])
+def Users(request,*args):
+    if request.method == 'GET':
+        queryset   = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return JsonResponse(serializer.data,  safe=False)
+
+# To Register  
+@api_view(['POST'])
+def UserRegister(request,*args):
+    if request.method=='POST':
+        try:
+            user = request.data['username']
+            if User.objects.filter(username=user).exists():
+                return Response("Username already exists")
+            else:
+                serializer = UserSerializer(data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=201)
+
+        except:
+            return HttpResponse(status=404)
 
 # To Login
 @api_view(['POST'])
 def Userlogin(request,*args):
     if request.method=='POST':
         try:
-            print("\n\n")
-            print(request)
-            print(request.data['username'])
+            # In whatever situation json response should be given.. that is why double try block
             try:
                 queryset = User.objects.get(username=request.data['username'])
-                print(queryset)
-                print("check")
                 flag = {"login": "true"}
             except:
-                print("No data")
                 flag = {"login": "false"}
 
         except:
             return HttpResponse(status=404)
         return Response(flag)
 
-#To retrieve all meetings
+#To Search users while creating room
 @api_view(['POST'])
 def UserSearch(request,*args):
     if request.method=='POST':
         try:
             data = []
-            
-            print("\n\n")
-            print(request)
-            print("dcds")
-            print(request.data['username'])
-            print("ese")
             queryset = User.objects.filter(username__contains="{}".format(request.data['username']))
-            print(queryset)
-            #serializer = UserSerializer(queryset)
-            print("\n\n")
             for i in queryset:
                 value = {
                     "username"  :"",
                     "name"      :"",
                     "email"     :""
                 }
-                serializer = UserSerializer(i)
-                print("\n\n\n\n\n ----------------------------------------------------------")
-                print(serializer.data)
-                print(serializer.data['first_name'])
-                print(serializer.data['email'])
-                print("value dict before -",value)            
+                serializer = UserSerializer(i)           
                 value["username"]   = serializer.data['username']
                 value["name"]       = serializer.data['first_name']
                 value["email"]      = serializer.data['email'] 
-                print("value dict after -",value)            
                 data.append(value)
-                print(data)
-            
-
-            print("\n",data)
-            #data = json.loads(data)
-            #print("\n",data)
             return Response(data)
             
-            
         except:
-            print("NOT")
             return HttpResponse(status=404)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#To set meeting
+@api_view(['POST'])
+def MeetingSet(request,*args):
+    if request.method=='POST':
+        try:
+            meeting = request.data
+            users   = request.data['persons']
+
+            server_meeting = {
+            "room_name"      : "","start_time"         : "","end_time"           : "",
+            "host"           : "","host_name"          : "","host_email"         : "",
+            "participant_2"  : "none", "participant_2_name": "none", "participant_2_email": "none@gmail.com",
+            "participant_3"  : "none", "participant_3_name": "none", "participant_3_email": "none@gmail.com",
+            "participant_4"  : "none", "participant_4_name": "none", "participant_4_email": "none@gmail.com",
+            }
+
+            try:
+                server_meeting['room_name']  = meeting['name']
+                server_meeting['start_time'] = meeting['start']
+                server_meeting['end_time']   = meeting['end']
+                
+                server_meeting['host']       = users[0]['username']
+                server_meeting['host_name']  = users[0]['name']
+                server_meeting['host_email'] = users[0]['email']
+
+                server_meeting['participant_2']       = users[1]['username']
+                server_meeting['participant_2_name']  = users[1]['name']
+                server_meeting['participant_2_email'] = users[1]['email']
+
+                server_meeting['participant_3']       = users[2]['username']
+                server_meeting['participant_3_name']  = users[2]['name']
+                server_meeting['participant_3_email'] = users[2]['email']
+
+                server_meeting['participant_4']       = users[3]['username']
+                server_meeting['participant_4_name']  = users[3]['name']
+                server_meeting['participant_4_email'] = users[3]['email']
+
+            except:    
+                serializer = MeetingsSerializer(data=server_meeting)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+            
+            return HttpResponse("Not Valid")
+                    
+        except:
+            return HttpResponse(status=404)
+
+
+
+
+
+
+
 
 #To retrieve All meetings
 @api_view(['POST'])
@@ -145,49 +212,24 @@ def MeetingInvited(request,*args):
         except:
             return HttpResponse(status=404)
 
-# To Register  
-@api_view(['POST','GET'])
-def UserRegister(request,*args):
-    if request.method=='POST':
-        try:
-            print("\n\n")
-            print(request)
-            print(request.data)
-            print(request.data['username'])
-            user = request.data['username']
-            if User.objects.filter(username=user).exists():
-                return Response("Username already exists")
-            else:
-                serializer = UserSerializer(data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=201)
-
-        except:
-            return HttpResponse(status=404)
-    if request.method == 'GET':
-        queryset = User.objects.all()
-        print(queryset)
-        serializer = UserSerializer(queryset, many=True)
-        print(serializer)
-        return JsonResponse(serializer.data, safe=False)
 
 
-#Class based views for less code
-""" class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer """
+
+
+
+
+
+
+#Class based views for less code    
 
 class MeetingList(generics.ListCreateAPIView):
     queryset = Meetings.objects.all()
     serializer_class = MeetingsSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = UserSerializer
-    
-    def retrieve(self, username):
-    
-        queryset = User.objects.get(username=username)
+    lookup_field = "username"
+
+    def retrieve(self, request, username):
+        queryset   = User.objects.get(username=username)
         serializer = UserSerializer(queryset)        
         return Response(serializer.data)
-
